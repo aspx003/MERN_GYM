8350cd365c18983f66c06c1c00ef2c42cf4b6eea
Ansuman Patra <ansupatra003@gmail.com>
2024-07-03 20:21:19 +0530

    Database connected(local)

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,29 @@
-const express = require('express');
-require('dotenv').config();
-const workoutRoutes = require("./routes/workouts.js")
+const express = require("express");
+require("dotenv").config();
+const workoutRoutes = require("./routes/workouts.js");
+const mongoose = require("mongoose");
 
 // express app
 const app = express();
 const PORT = process.env.PORT;
+const DB = process.env.MONGO_URI;
 app.use(express.json());
 app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    next();
-})
+  console.log(req.path, req.method);
+  next();
+});
 
-app.use('/api/workouts', workoutRoutes);
+app.use("/api/workouts", workoutRoutes);
 
-// listen for requests
-app.listen(PORT, () => {
-    console.log('listening on port 4000');
-})
+// connect to the database
+mongoose
+  .connect(DB)
+  .then(() => {
+    // listen for requests
+    app.listen(PORT, () => {
+      console.log("Connected to the port & listening on port", PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
